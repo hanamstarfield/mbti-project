97a7cf8375a7c39295ef187cd659957bce59904f
hanamstarfield <qnfdmlto@naver.com>
2024-09-11 10:19:51 +0900

    update: zustand 미들웨어 사용

--- src/zustand/useUserStore.js
+++ src/zustand/useUserStore.js
@@ -1,20 +1,38 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useUserStore = create((set) => ({
-  user: JSON.parse(sessionStorage.getItem("user")) || null,
-  loginUser: (userData) => {
-    sessionStorage.setItem("user", JSON.stringify(userData));
-    set({ user: userData });
-  },
-  updateUserProfile: (updatedData) =>
-    set((state) => {
-      const updatedUser = { ...state.user, ...updatedData };
-      sessionStorage.setItem("user", JSON.stringify(updatedUser));
-      return { user: updatedUser };
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      loginUser: (userData) => set({ user: userData }),
+      updateUserProfile: (updatedData) =>
+        set((state) => ({ user: { ...state.user, ...updatedData } })),
+      logoutUser: () => set({ user: null }),
     }),
-  logoutUser: () => {
-    sessionStorage.removeItem("user");
-    set({ user: null });
-  },
-}));
+    {
+      name: "user-storage",
+      getStorage: () => sessionStorage,
+    }
+  )
+);
+
+//   (set) => ({
+//   user: JSON.parse(sessionStorage.getItem("user")) || null,
+//   loginUser: (userData) => {
+//     sessionStorage.setItem("user", JSON.stringify(userData));
+//     set({ user: userData });
+//   },
+//   updateUserProfile: (updatedData) =>
+//     set((state) => {
+//       const updatedUser = { ...state.user, ...updatedData };
+//       sessionStorage.setItem("user", JSON.stringify(updatedUser));
+//       return { user: updatedUser };
+//     }),
+//   logoutUser: () => {
+//     sessionStorage.removeItem("user");
+//     set({ user: null });
+//   },
+// }));
+
 export { useUserStore };
